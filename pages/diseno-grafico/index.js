baec20196c2ac7a6ad0e4e260ac09b665556b13c
sie.rra <rudiazbern@gmail.com>
2023-08-25 11:05:51 +0200

    feat: add scroll snap to pages

--- pages/diseno-grafico/index.js
+++ pages/diseno-grafico/index.js
@@ -37,157 +37,151 @@ export default function DisenoGrafico() {
 						className="bg-[url(/portfolio/img/ProyectosDGHiFi.svg)] dark:bg-[url(/portfolio/img/ProyectosDGLoFi.svg)] bg-no-repeat m-0 bg-cover w-screen">
 						<div>
 							<ProyectHeader />
-							<section className="h-[70vh] mb-[30vh] md:mt-[10vh] mx-8 text-center flex items-center justify-center md:items-start">
-								<div className="flex flex-col gap-10 md:gap-20">
-									<div className="mt-10">
-										<Title notChangeStyle>Proyectos</Title>
-										<div className="block mt-2">
+							<div className="snap snap-y snap-mandatory h-screen w-screen overflow-y-scroll">
+								<section className="h-[70vh] mb-[30vh] md:mt-[10vh] mx-8 text-center flex items-center justify-center md:items-start snap-start">
+									<div className="flex flex-col gap-10 md:gap-20">
+										<div className="mt-10">
 											<Title notChangeStyle>
-												Diseño Grafico
+												Proyectos
 											</Title>
+											<div className="block mt-2">
+												<Title notChangeStyle>
+													Diseño Grafico
+												</Title>
+											</div>
+										</div>
+										<div className="flex justify-center items-center">
+											<div className="grid grid-cols-2 lg:grid-cols-4 justify-items-center md:px-[5vw] gap-8 md:w-full justify-center">
+												{proyectsDisenoGrafico.map(
+													(proyect, index) => {
+														return (
+															<CardButton
+																key={
+																	proyect.title
+																}
+																title={
+																	proyect.title
+																}
+																date={
+																	proyect.date
+																}
+																isAlternative={
+																	index %
+																		2 ===
+																	0
+																}
+																isLofi={isLofi}
+																routeToGo={`#${proyect.title}`}
+															/>
+														)
+													}
+												)}
+											</div>
 										</div>
 									</div>
-									<div className="flex justify-center items-center">
-										<div className="grid grid-cols-2 lg:grid-cols-4 justify-items-center md:px-[5vw] gap-8 md:w-full justify-center">
-											{proyectsDisenoGrafico.map(
-												(proyect, index) => {
-													return (
-														<CardButton
-															key={proyect.title}
-															title={
-																proyect.title
-															}
-															date={proyect.date}
-															isAlternative={
-																index % 2 === 0
-															}
-															isLofi={isLofi}
-															routeToGo={`#${proyect.title}`}
-														/>
-													)
-												}
-											)}
-										</div>
-									</div>
-								</div>
-							</section>
-							<div>
-								{proyectsDisenoGrafico.map((proyect, index) => {
-									const prevProyect =
-										proyectsDisenoGrafico[index - 1]
-									const nextProyect =
-										proyectsDisenoGrafico[index + 1]
+								</section>
+								<div>
+									{proyectsDisenoGrafico.map(
+										(proyect, index) => {
+											const prevProyect =
+												proyectsDisenoGrafico[index - 1]
+											const nextProyect =
+												proyectsDisenoGrafico[index + 1]
 
-									const {
-										title,
-										imageSrc,
-										duration,
-										role,
-										typeOfWork,
-										content,
-										behanceLink,
-										externalLink
-									} = proyect
+											const {
+												title,
+												imageSrc,
+												duration,
+												role,
+												typeOfWork,
+												content,
+												behanceLink,
+												externalLink
+											} = proyect
 
-									return (
-										<SectionItem
-											key={title}
-											id={title}
-											title={title}
-											isImageInRight={index % 2 !== 0}
-											imageSrc={imageSrc}
-											duration={duration}
-											role={role}
-											typeOfWork={typeOfWork}
-											content={content}
-											behanceLink={behanceLink}
-											externalLink={externalLink}
-											prevProyect={
-												prevProyect
-													? prevProyect.title
-													: 'Proyectos'
-											}
-											prevProyectHref={
-												prevProyect
-													? `#${prevProyect.title}`
-													: '#proyectos'
-											}
-											nextProyect={
-												nextProyect
-													? nextProyect.title
-													: '¡Contacta Conmigo!'
-											}
-											nextProyectHref={
-												nextProyect
-													? `#${nextProyect.title}`
-													: '#contacta'
-											}
-										/>
-									)
-								})}
-							</div>
-							<motion.div
-								initial={{ opacity: 0 }}
-								whileInView={{ opacity: 1 }}
-								transition={{ duration: 1 }}
-								className="w-full relative flex justify-center items-start -mt-3">
-								<a
-									href={`#${
-										proyectsDisenoGrafico[
-											proyectsDisenoGrafico.length - 1
-										].title
-									}`}>
-									<ButtonSection
-										text={
-											proyectsDisenoGrafico[
-												proyectsDisenoGrafico.length - 1
-											].title
+											return (
+												<SectionItem
+													key={title}
+													id={title}
+													title={title}
+													isImageInRight={
+														index % 2 !== 0
+													}
+													imageSrc={imageSrc}
+													duration={duration}
+													role={role}
+													typeOfWork={typeOfWork}
+													content={content}
+													behanceLink={behanceLink}
+													externalLink={externalLink}
+													prevProyect={
+														prevProyect
+															? prevProyect.title
+															: 'Proyectos'
+													}
+													prevProyectHref={
+														prevProyect
+															? `#${prevProyect.title}`
+															: '#proyectos'
+													}
+													nextProyect={
+														nextProyect
+															? nextProyect.title
+															: '¡Contacta Conmigo!'
+													}
+													nextProyectHref={
+														nextProyect
+															? `#${nextProyect.title}`
+															: '#contacta'
+													}
+												/>
+											)
 										}
-										isPrev="true"
-									/>
-								</a>
-							</motion.div>
-							<section
-								className="h-[80vh] flex flex-col justify-center md:justify-start"
-								id="contacta">
-								<div className="w-full pt-0 md:pt-40  mx-auto text-center">
-									<span className="font-serif px-2 md:px-0 text-6xl md:text-8xl transition-all duration-200">
-										¡Contacta conmigo!
-									</span>
+									)}
 								</div>
-								<div className="flex gap-5  h-5 mt-24 justify-center items-center">
-									<SocialButton
-										bg="/portfolio/icons/Linkedin.svg"
-										bgLofi="/portfolio/icons/LinkedinLo.svg"
-										href="https://www.linkedin.com/in/natalia-del-barco-rojas-product-designer/"
-										isLofi={isLofi}
-									/>
-									<SocialButton
-										bg="/portfolio/icons/Medium.svg"
-										bgLofi="/portfolio/icons/MediumLo.svg"
-										href="https://medium.com/@nataliadelbarcorojas"
-										isLofi={isLofi}
-									/>
-									<SocialButton
-										bg="/portfolio/icons/mail.svg"
-										bgLofi="/portfolio/icons/MailLo.svg"
-										href="mailto:nataliadelbarcodesign@gmail.com"
-										isLofi={isLofi}
-									/>
-									<SocialButton
-										bg="/portfolio/icons/Behance.svg"
-										bgLofi="/portfolio/icons/BehanceLo.svg"
-										href="https://www.behance.net/nataliadelbar"
-										isLofi={isLofi}
-									/>
-									<SocialButton
-										bg="/portfolio/icons/Figma.svg"
-										bgLofi="/portfolio/icons/FigmaLo.svg"
-										href="https://www.figma.com/@nataliadelbarco"
-										isLofi={isLofi}
-									/>
-								</div>
-							</section>
+
+								<section
+									className="md:pt-0 h-[100vh] md:h-[100vh] my-[5vh] md:my-0 flex flex-col justify-center snap-start"
+									id="contacta">
+									<div className="w-full pt-0 md:pt-0 mx-auto text-center">
+										<span className="font-serif px-2 md:px-0 text-6xl md:text-8xl transition-all duration-200">
+											¡Contacta conmigo!
+										</span>
+									</div>
+									<div className="flex gap-5 h-5 md:h-auto mt-24 justify-center items-center">
+										<SocialButton
+											bg="/portfolio/icons/Linkedin.svg"
+											bgLofi="/portfolio/icons/LinkedinLo.svg"
+											href="https://www.linkedin.com/in/natalia-del-barco-rojas-product-designer/"
+											isLofi={isLofi}
+										/>
+										<SocialButton
+											bg="/portfolio/icons/Medium.svg"
+											bgLofi="/portfolio/icons/MediumLo.svg"
+											href="https://medium.com/@nataliadelbarcorojas"
+											isLofi={isLofi}
+										/>
+										<SocialButton
+											bg="/portfolio/icons/mail.svg"
+											bgLofi="/portfolio/icons/MailLo.svg"
+											href="mailto:nataliadelbarcodesign@gmail.com"
+											isLofi={isLofi}
+										/>
+										<SocialButton
+											bg="/portfolio/icons/Behance.svg"
+											bgLofi="/portfolio/icons/BehanceLo.svg"
+											href="https://www.behance.net/nataliadelbar"
+											isLofi={isLofi}
+										/>
+										<SocialButton
+											bg="/portfolio/icons/Figma.svg"
+											bgLofi="/portfolio/icons/FigmaLo.svg"
+											href="https://www.figma.com/@nataliadelbarco"
+											isLofi={isLofi}
+										/>
+									</div>
+								</section>
+							</div>
 						</div>
 					</motion.main>
 				</div>
--- pages/index.js
+++ pages/index.js
@@ -50,125 +50,160 @@ export default function Home() {
 								setIsToggleMenu(!isToggleMenu)
 							}
 						/>
-						{/* Portada */}
-						<Section
-							nextSection="Sobre mí"
-							nextSectionHref="#sobre-mi"
-							id="intro"
-							isFirstSection={true}>
-							<motion.h1
-								{...animationOpacitySettings}
-								transition={{ delay: 1, duration: 2 }}
-								className="font-serif text-4xl text-center pt-32 md:pt-0 md:text-start md:text-8xl dark:text-transparent transition-all duration-200"
-								style={textStyle}>
-								Portfolio <br />
-								Natalia del Barco
-							</motion.h1>
-						</Section>
-						{/* Sobre mi */}
-						<Section
-							prevSection="Intro"
-							prevSectionHref="#intro"
-							nextSection="Proyectos"
-							nextSectionHref="#proyectos"
-							id="sobre-mi">
-							<div>
-								<div className="flex flex-col md:flex-row items-center justify-center h-full">
-									<div className="w-full md:h-[60vh] -mt-8 md:mt-0 md:w-1/2">
-										<div className="md:pt-4">
-											<Title>Sobre mí</Title>
-										</div>
+						<div className="snap snap-y snap-mandatory h-screen w-screen overflow-y-scroll">
+							{/* Portada */}
+							<Section
+								nextSection="Sobre mí"
+								nextSectionHref="#sobre-mi"
+								id="intro"
+								isFirstSection={true}>
+								<motion.h1
+									{...animationOpacitySettings}
+									transition={{ delay: 1, duration: 2 }}
+									className="font-serif text-4xl text-center pt-32 md:pt-0 md:text-start md:text-8xl dark:text-transparent transition-all duration-200"
+									style={textStyle}>
+									Portfolio <br />
+									Natalia del Barco
+								</motion.h1>
+							</Section>
+							{/* Sobre mi */}
+							<Section
+								prevSection="Intro"
+								prevSectionHref="#intro"
+								nextSection="Proyectos"
+								nextSectionHref="#proyectos"
+								id="sobre-mi">
+								<div>
+									<div className="flex flex-col md:flex-row items-center justify-center h-full">
+										<div className="w-full md:h-[60vh] -mt-8 md:mt-0 md:w-1/2">
+											<div className="pt-4">
+												<Title>Sobre mí</Title>
+											</div>
 
-										<p className="md:mr-12 pt-4 pr-4 md:pt-10 md:h-[55vh] h-[37vh] overflow-y-auto pb-[2vh] text-sm md:text-lg lg:text-md sobreMi">
-											<span>¡Hola!</span>
-										<br/>
-										Me llamo Natalia y entre otras muchas cosas soy{' '}
-										<b className="font-bold">
-											Diseñadora de Producto UX/UI,{' '}
-										</b>
-										desde hace 6 años me he centrado en estudiar el diseño y comprender cómo comunicar al usuario de una forma perspicaz y visual. Mi cometido es ayudar a gestionar una experiencia de usuario e interfaz exitosa en el menor tiempo posible, realizando un trabajo interdisciplinar, centrado en el ser humano. He trabajado en proyectos para particulares y empresas; en la industria de la publicidad, marketing, comunicación, farmacéutica y de energía.
-										Gracias a los conocimientos en Diseño Gráfico, pongo en uso toda la creatividad y conocimientos prácticos en mi trabajo.
-										Para saber un poquito más sobre mí tendrás que saber que soy amante de todos los animales (excepto bichos voladores) y casa de acogida, cocinera experta, gamer, friki de la tecnología, IA, domótica y diseño, hago trekking en entornos naturales y urbanos y  la fotografía, que me acompaña a donde vaya de viaje.
-										¿Lo que más me motiva de mi trabajo? el poder mejorar la experiencia del usuario al interactuar con el producto, con un diseño bien enfocado, visual y perspicaz.
-									</p>
+											<p className="md:mr-12 pt-4 pr-4 md:pt-10 md:h-[55vh] h-[30vh] overflow-y-auto pb-[2vh] text-sm md:text-lg lg:text-md sobreMi">
+												<span>¡Hola!</span>
+												<br />
+												Me llamo Natalia y entre otras
+												muchas cosas soy{' '}
+												<b className="font-bold">
+													Diseñadora de Producto
+													UX/UI,{' '}
+												</b>
+												desde hace 6 años me he centrado
+												en estudiar el diseño y
+												comprender cómo comunicar al
+												usuario de una forma perspicaz y
+												visual. Mi cometido es ayudar a
+												gestionar una experiencia de
+												usuario e interfaz exitosa en el
+												menor tiempo posible, realizando
+												un trabajo interdisciplinar,
+												centrado en el ser humano. He
+												trabajado en proyectos para
+												particulares y empresas; en la
+												industria de la publicidad,
+												marketing, comunicación,
+												farmacéutica y de energía.
+												Gracias a los conocimientos en
+												Diseño Gráfico, pongo en uso
+												toda la creatividad y
+												conocimientos prácticos en mi
+												trabajo. Para saber un poquito
+												más sobre mí tendrás que saber
+												que soy amante de todos los
+												animales (excepto bichos
+												voladores) y casa de acogida,
+												cocinera experta, gamer, friki
+												de la tecnología, IA, domótica y
+												diseño, hago trekking en
+												entornos naturales y urbanos y
+												la fotografía, que me acompaña a
+												donde vaya de viaje. ¿Lo que más
+												me motiva de mi trabajo? el
+												poder mejorar la experiencia del
+												usuario al interactuar con el
+												producto, con un diseño bien
+												enfocado, visual y perspicaz.
+											</p>
+										</div>
+										<div className="flex items-center mt-[4vh] md:-mt-14 md:my-0 w-full md:w-1/2 h-full justify-center md:justify-between mx-auto md:ml-32">
+											<div className="bg-[url('/portfolio/img/PerfilHiFi.png')] dark:bg-[url('/portfolio/img/PerfilLoFi.webp')] h-48 w-48 md:h-[30rem] md:w-[30rem] bg-contain bg-no-repeat transition-all duration-200"></div>
+										</div>
+									</div>
 								</div>
-								<div className="flex items-center mt-[4vh] md:-mt-14 md:my-0 w-full md:w-1/2 h-full justify-center md:justify-between mx-auto md:ml-32">
-									<div className="bg-[url('/portfolio/img/PerfilHiFi.png')] dark:bg-[url('/portfolio/img/PerfilLoFi.webp')] h-48 w-48 md:h-[30rem] md:w-[30rem] bg-contain bg-no-repeat transition-all duration-200"></div>
+							</Section>
+							{/* Proyectos */}
+							<Section
+								prevSection="Sobre Mí"
+								prevSectionHref="#sobre-mi"
+								nextSection="Contacta Conmigo"
+								nextSectionHref="#contacta-conmigo"
+								id="proyectos">
+								<div className="w-full flex justify-center">
+									<Title>Proyectos</Title>
 								</div>
-							</div>
+								<div className="w-full h-full flex md:flex-row flex-col justify-evenly md:justify-center md:gap-12 md:-mt-5 items-center">
+									<CardButton
+										isAlternative="true"
+										title="UX/UI"
+										isLofi={lofiToggle}
+										hasGoToOtherPage
+										routeToGo="/ui-ux"
+										isCardProject
+									/>
+									<CardButton
+										title="Diseño Gráfico"
+										isLofi={lofiToggle}
+										hasGoToOtherPage
+										routeToGo="/diseno-grafico"
+										isCardProject
+									/>
+								</div>
+							</Section>
+							<Section
+								prevSection="Proyectos"
+								prevSectionHref="#proyectos"
+								id="contacta-conmigo">
+								<div className="w-full pt-20 mx-auto text-center">
+									<Title>¡Contacta conmigo!</Title>
+								</div>
+								<div className="flex gap-5  h-5 mt-24 justify-center items-center">
+									<SocialButton
+										bg="/portfolio/icons/Linkedin.svg"
+										bgLofi="/portfolio/icons/LinkedinLo.svg"
+										isLofi={lofiToggle}
+										href="https://www.linkedin.com/in/natalia-del-barco-rojas-product-designer/"
+									/>
+									<SocialButton
+										bg="/portfolio/icons/Medium.svg"
+										bgLofi="/portfolio/icons/MediumLo.svg"
+										isLofi={lofiToggle}
+										href="https://medium.com/@nataliadelbarcorojas"
+									/>
+									<SocialButton
+										bg="/portfolio/icons/mail.svg"
+										bgLofi="/portfolio/icons/MailLo.svg"
+										isLofi={lofiToggle}
+										href="mailto:nataliadelbarcodesign@gmail.com"
+									/>
+									<SocialButton
+										bg="/portfolio/icons/Behance.svg"
+										bgLofi="/portfolio/icons/BehanceLo.svg"
+										isLofi={lofiToggle}
+										href="https://www.behance.net/nataliadelbarcorojas"
+									/>
+									<SocialButton
+										bg="/portfolio/icons/Figma.svg"
+										bgLofi="/portfolio/icons/FigmaLo.svg"
+										isLofi={lofiToggle}
+										href="https://www.figma.com/@nataliadelbarco"
+									/>
+								</div>
+							</Section>
 						</div>
-					</Section>
-					{/* Proyectos */}
-					<Section
-						prevSection="Sobre Mí"
-						prevSectionHref="#sobre-mi"
-						nextSection="Contacta Conmigo"
-						nextSectionHref="#contacta-conmigo"
-						id="proyectos">
-						<div className="w-full flex justify-center">
-							<Title>Proyectos</Title>
-						</div>
-						<div className="w-full h-full flex md:flex-row flex-col justify-evenly md:justify-center md:gap-12 md:-mt-5 items-center">
-							<CardButton
-								isAlternative="true"
-								title="UX/UI"
-								isLofi={lofiToggle}
-								hasGoToOtherPage
-								routeToGo="/ui-ux"
-								isCardProject
-							/>
-							<CardButton
-								title="Diseño Gráfico"
-								isLofi={lofiToggle}
-								hasGoToOtherPage
-								routeToGo="/diseno-grafico"
-								isCardProject
-							/>
-						</div>
-					</Section>
-					<Section
-						prevSection="Proyectos"
-						prevSectionHref="#proyectos"
-						id="contacta-conmigo">
-						<div className="w-full pt-20 mx-auto text-center">
-							<Title>¡Contacta conmigo!</Title>
-						</div>
-						<div className="flex gap-5  h-5 mt-24 justify-center items-center">
-							<SocialButton
-								bg="/portfolio/icons/Linkedin.svg"
-								bgLofi="/portfolio/icons/LinkedinLo.svg"
-								isLofi={lofiToggle}
-								href="https://www.linkedin.com/in/natalia-del-barco-rojas-product-designer/"
-							/>
-							<SocialButton
-								bg="/portfolio/icons/Medium.svg"
-								bgLofi="/portfolio/icons/MediumLo.svg"
-								isLofi={lofiToggle}
-								href="https://medium.com/@nataliadelbarcorojas"
-							/>
-							<SocialButton
-								bg="/portfolio/icons/mail.svg"
-								bgLofi="/portfolio/icons/MailLo.svg"
-								isLofi={lofiToggle}
-								href="mailto:nataliadelbarcodesign@gmail.com"
-							/>
-							<SocialButton
-								bg="/portfolio/icons/Behance.svg"
-								bgLofi="/portfolio/icons/BehanceLo.svg"
-								isLofi={lofiToggle}
-								href="https://www.behance.net/nataliadelbarcorojas"
-							/>
-							<SocialButton
-								bg="/portfolio/icons/Figma.svg"
-								bgLofi="/portfolio/icons/FigmaLo.svg"
-								isLofi={lofiToggle}
-								href="https://www.figma.com/@nataliadelbarco"
-							/>
-						</div>
-					</Section>
-				</main>
-			</motion.div>
-		</AnimatePresence >
+					</main>
+				</motion.div>
+			</AnimatePresence>
 		</>
 	)
 }
